c2f8a034fb6d93a947381f13d5a1443b7aff10f9
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2025-06-28 01:29:12 +0330

    setup payment

---
+++ apps/payments/src/payments.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { PaymentsController } from './payments.controller';
+import { PaymentsService } from './payments.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi'
+import { LoggerModule } from '@app/common';
+import Stripe from 'stripe';
+
+@Module({
+  imports: [
+     ConfigModule.forRoot({
+          isGlobal: true,
+          validationSchema: Joi.object({
+            PORT:Joi.number().required(),
+            STRIPE_SECRET_KEY:Joi.string().required(),
+          }),
+        }),
+        Stripe,
+        LoggerModule
+  ],
+  controllers: [PaymentsController],
+  providers: [PaymentsService],
+})
+export class PaymentsModule {}
---
+++ apps/payments/src/payments.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+
+@Injectable()
+export class PaymentsService {
+     private stripe: Stripe;
+
+  constructor(private readonly configService: ConfigService) {
+    this.stripe = new Stripe(this.configService.get<string>('STRIPE_SECRET_KEY')!, 
+    {
+      apiVersion: '2025-05-28.basil',
+    });
+  }
+
+
+
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
